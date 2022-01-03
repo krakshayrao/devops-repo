29738e146d580e34d2073b26f4d892a6fcec2f73
Akshay Ravindra <akshay.ravindra@cat.com>
2022-01-03 14:15:30 +0530

    changed conf.py root_doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,4 +54,4 @@ html_theme = 'furo'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-master_doc = 'contents'  #Aded  for readthedocs
+root_doc = 'index'  #Aded  for readthedocs
